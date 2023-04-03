ff0570a965f855ce30d1a0a909d9fbaa6d95a21c
RyokCR <ryokwr@gmail.com>
2023-04-03 10:44:47 -0400

    fix test

--- test/bloc_state_test.dart
+++ test/bloc_state_test.dart
@@ -1,16 +1,12 @@
 
 import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_app/auth/login_register/form_submission_status.dart';
 import 'package:flutter_test_app/auth/login_register/login_register_bloc.dart';
 import 'package:flutter_test_app/auth/login_register/login_register_event.dart';
 import 'package:flutter_test_app/auth/login_register/login_register_state.dart';
 import 'package:flutter_test_app/model/entities/user.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 
 
 
